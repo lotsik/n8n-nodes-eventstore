de836d77d5c5bca9ea16b261f81319b896d82947
Andrew B <lotsik@mail.ru>
2024-02-23 23:28:54 +0300

    fixes

--- nodes/EventStore/EventStore.node.ts
+++ nodes/EventStore/EventStore.node.ts
@@ -7,7 +7,7 @@ import {
 import { EventStoreDBClient, SingleNodeOptions, jsonEvent, START, FORWARDS, JSONEventType, Credentials, ChannelCredentialOptions } from '@eventstore/db-client';
 import { v4 } from 'uuid';
 
-export class EventStoreNode implements INodeType {
+export class EventStore implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'EventStore Node',
 		name: 'eventStoreNode',
